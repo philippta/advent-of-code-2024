0e2cf30cc328d06673c5b6b434197ccebff231d0
Philipp Tanlak <philipp.tanlak@gmail.com>
2024-12-01 22:07:28 +0100

    day 1, part 2

--- day1.c
+++ day1.c
@@ -44,4 +44,18 @@ int main(void) {
 	}
 
 	printf("Part 1: %d\n", diff);
+
+	int sum = 0;
+	for (size_t i = 0; i < nlines; i++) {
+		int count = 0;
+		for (size_t j = 0; j < nlines; j++) {
+			if (left[i] == right[j]) {
+				count++;
+			}
+		}
+
+		sum += left[i] * count;
+	}
+
+	printf("Part 2: %d\n", sum);
 }
